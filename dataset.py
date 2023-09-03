f21ce122dee0ac7e0634f8072f707573975f708f
braceal <braceal@umich.edu>
2023-09-03 11:44:43 -0500

    dataset logi

---
+++ dataset.py
@@ -0,0 +1,78 @@
+import re
+from pathlib import Path
+from typing import Any, Dict, List
+
+from torch.utils.data import Dataset
+from transformers import BatchEncoding, DataCollatorForLanguageModeling
+
+
+def group_codons(seq: str) -> str:
+    return " ".join(seq[i : i + 3] for i in range(0, len(seq), 3)).upper()
+
+def read_fasta_only_seq(fasta_file: str) -> List[str]:
+    """Reads fasta file sequences without description tag."""
+    text = Path(fasta_file).read_text()
+    pattern = re.compile("^>", re.MULTILINE)
+    non_parsed_seqs = re.split(pattern, text)[1:]
+    lines = [
+        line.replace("\n", "")
+        for seq in non_parsed_seqs
+        for line in seq.split("\n", 1)
+    ]
+    return lines[1::2]
+
+class FastaDataset(Dataset):
+    def __init__(self, file_path: str) -> None:
+        # Read the fasta file
+        dna_sequenes = read_fasta_only_seq(file_path)
+        # Preprocess the sequences into codons
+        # TODO: We could also use an <unk> token (this would be better)
+        self.sequences = [
+            group_codons(seq) for seq in dna_sequenes if len(seq) % 3 == 0
+        ]
+
+    def __len__(self) -> int:
+        return len(self.sequences)
+
+    def __getitem__(self, idx: int) -> Dict[str, str]:
+        # Get the idx'th codon sequence
+        return {"codon": self.sequences[idx]}
+
+
+class GenSLMColatorForLanguageModeling(DataCollatorForLanguageModeling):
+    """Augment the underlying DataCollatorForLanguageModeling to handle
+    multiple batch encoding inputs."""
+
+    def __init__(self, train_mode: bool = False, **kwargs) -> None:
+        self.train_mode = train_mode
+        super().__init__(**kwargs)
+
+    def tokenize(self, sequences: List[str]) -> BatchEncoding:
+        return self.tokenizer(
+            sequences,
+            return_tensors="pt",
+            truncation=True,
+            padding=True,
+            return_special_tokens_mask=self.train_mode and self.mlm,
+        )
+
+    def torch_call(self, examples: List[Dict[str, str]]) -> Dict[str, Any]:
+        # First, tokenize the batch
+        batch = self.tokenize([e["codon"] for e in examples])
+
+        # We only need to mask tokens if we are training
+        if not self.train_mode:
+            return batch
+
+        if self.mlm:
+            # If special token mask has been preprocessed, pop it from the dict.
+            batch["input_ids"], batch["labels"] = self.torch_mask_tokens(
+                batch["input_ids"],
+                special_tokens_mask=batch.pop("special_tokens_mask", None),
+            )
+        else:
+            labels = batch["input_ids"].clone()
+            if self.tokenizer.pad_token_id is not None:
+                labels[labels == self.tokenizer.pad_token_id] = -100
+            batch["labels"] = labels
+        return batch
