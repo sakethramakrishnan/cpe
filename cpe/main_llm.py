7f682d7254677c27141c484225a53a6d8cc5c733
braceal <braceal@umich.edu>
2023-11-17 18:08:09 -0600

    refactor tokenizer settings

--- cpe/main_llm.py
+++ cpe/main_llm.py
@@ -1,26 +1,20 @@
-# TODO: scale the vocab size with bpe and tune it
-# 
-
-from argparse import ArgumentParser
 import os
+from argparse import ArgumentParser
+from dataclasses import asdict, dataclass
 from pathlib import Path
-
-from dataset import FastaDataset, GenSLMCollatorForLanguageModeling
-
-from transformers.trainer_utils import get_last_checkpoint
-
-from transformers import (
-    BertForMaskedLM,
-    GPTNeoXForCausalLM,
-    PreTrainedTokenizerFast,
-    Trainer,
-    TrainingArguments,
-    PretrainedConfig
-)
 
 import wandb
 import yaml
-from dataclasses import asdict, dataclass
+from dataset import FastaDataset, GenSLMCollatorForLanguageModeling
+from transformers import (
+    BertForMaskedLM,
+    GPTNeoXForCausalLM,
+    PretrainedConfig,
+    PreTrainedTokenizerFast,
+    Trainer,
+    TrainingArguments,
+)
+from transformers.trainer_utils import get_last_checkpoint
 
 os.environ["WANDB_DISABLED"] = "true"
 
@@ -35,20 +29,21 @@ MODEL_DISPATCH = {
 # this dataclass consolidates the training configuration
 @dataclass
 class GenSLMTrainingConfig:
-    
     num_train_epochs: int = 20
     per_device_train_batch_size: int = 64
     per_device_eval_batch_size: int = 128
     gradient_accumulation_steps: int = 2
     model_architecture: str = ""
     model_path: str = ""
-    tokenizer_path: str = "/home/couchbucks/Documents/saketh/LLM_sequences/test_tokenizer"
+    tokenizer_path: str = (
+        "/home/couchbucks/Documents/saketh/LLM_sequences/test_tokenizer"
+    )
     output_dir: str = "bpe_llm_out"
     train_path: str = "/home/couchbucks/Downloads/all_fasta_files/training/GCA_000977415.2_Sc_YJM1385_v1_genomic_extracted_sequences.fasta"
     validation_path: str = "/home/couchbucks/Downloads/all_fasta_files/training/GCA_000977415.2_Sc_YJM1385_v1_genomic_extracted_sequences.fasta"
-    evaluation_strategy: str = 'steps'
+    evaluation_strategy: str = "steps"
     eval_steps: int = 100
-    logging_strategy: str = 'steps'
+    logging_strategy: str = "steps"
     logging_steps: int = 500
     weight_decay: float = 0.01
     warmup_steps: int = 1000
@@ -57,13 +52,14 @@ class GenSLMTrainingConfig:
     save_steps: int = 500
     load_best_model_at_end: bool = True
     save_total_limit: int = 5
-    wandb_project: str = ''  # Set to empty string to turn off wandb
+    wandb_project: str = ""  # Set to empty string to turn off wandb
     fp16: bool = True
-    convert_to_aa: bool = ''  # whether to translate the DNA sequence into protein alphabets
-    num_char_per_token: int = ''  # how many characters per token
+    # whether to translate the DNA sequence into protein alphabets
+    tokenizer_type: str = "protein_alphabet_wordlevel"
+    convert_to_aa: bool = True
+    num_char_per_token: int = 1  # how many characters per token
 
     def __post_init__(self):
-
         # Setting this environment variable enables wandb logging
         if self.wandb_project:
             os.environ["WANDB_PROJECT"] = self.wandb_project
@@ -76,56 +72,38 @@ class GenSLMTrainingConfig:
         # Create the output directory if it doesn't exist
         Path(self.output_dir).mkdir(exist_ok=True, parents=True)
 
+        # Configure tokenization parameters
+        if self.tokenizer_type in ["ape_tokenizer", "protein_alphabet_wordlevel"]:
+            self.convert_to_aa = True
+            self.num_char_per_token = 1
+        elif self.tokenizer_type == ["npe_tokenizer", "dna_wordlevel"]:
+            self.convert_to_aa = False
+            self.num_char_per_token = 1
+        elif self.tokenizer_type in ["cpe_tokenizer", "codon_wordlevel"]:
+            self.convert_to_aa = False
+            self.num_char_per_token = 3
+        else:
+            raise ValueError(f"Invalid tokenizer_type: {self.tokenizer_type}")
+
         # Log the config to a yaml file
         with open(os.path.join(self.output_dir, "train_config.yaml"), "w") as fp:
             yaml.dump(asdict(self), fp)
 
 
 def main():
+    """
+    example usage in terminal format
+    python3 main_llm.py --config=/home/couchbucks/Documents/saketh/LLM_sequences/cpe/examples/training/train_config.yaml
+    """
+
     # Parse a yaml file to get the training config
     parser = ArgumentParser()
-
     parser.add_argument("--config", type=str, required=True)
-    parser.add_argument("--tokenizer_type", type=str, required=True, choices=['ape_tokenizer', 'npe_tokenizer', 'cpe_tokenizer', 'codon_wordlevel', 'dna_wordlevel', 'protein_alphabet_wordlevel'])
-    
-    '''
-    example usage in terminal format
-    python3 main_llm.py --config=/home/couchbucks/Documents/saketh/LLM_sequences/cpe/examples/training/train_config.yaml --tokenizer_type=ape_tokenizer
-    '''
-
 
     args = parser.parse_args()
     with open(args.config) as fp:
         config = GenSLMTrainingConfig(**yaml.safe_load(fp))
 
-
-    # The following checks if our input is congruent with the .yaml configuration
-    # adjust these settings in the yaml file
-    if args.tokenizer_type == "ape_tokenizer":
-        if config.convert_to_aa != True or config.num_char_per_token != 1:
-            raise ValueError("tokenizer type ape_tokenizer must have convert_to_aa=True and num_char_per_token=1. Check the config file or the tokenizer_type argument")
-    
-    elif args.tokenizer_type == "npe_tokenizer":
-        if config.convert_to_aa != False or config.num_char_per != 1:
-            raise ValueError("tokenizer type npe_tokenizer must have convert_to_aa=False and num_char_per_token=1. Check the config file or the token or the tokenizer_type argument")
-    
-    elif args.tokenizer_type == "cpe_tokenizer":
-        if config.convert_to_aa != False or config.num_char_per_token != 3:
-            raise ValueError("tokenizer type cpe_tokenizer must have convert_to_aa=False and num_char_per_token=3. Check the config file or the token or the tokenizer_type argument")
-    
-    elif args.tokenizer_type == "codon_wordlevel":
-        if config.convert_to_aa != False or config.num_char_per_token != 3:
-            raise ValueError("tokenizer type codon_wordlevel must have convert_to_aa=False and num_char_per_token=3. Check the config file or the token or the tokenizer_type argument")
-        
-    elif args.tokenizer_type == "dna_wordlevel":
-        if config.convert_to_aa != False or config.num_char_per_token != 1:
-            raise ValueError("tokenizer type dna_wordlevel must have convert_to_aa=False and num_char_per_token=1. Check the config file or the token or the tokenizer_type argument")
-    
-    elif args.tokenizer_type == "protein_alphabet_wordlevel":
-        if config.convert_to_aa!= True or config.num_char_per_token!= 1:
-            raise ValueError("tokenizer type protein_alphabet_wordlevel must have convert_to_aa=True and num_char_per_token=1. Check the config file or the token or the tokenizer_type argument")
-     
-    
     # HuggingFace API training parameters
     training_args = TrainingArguments(
         output_dir=config.output_dir,
@@ -150,45 +128,57 @@ def main():
     # Build Tokenizer
     if os.path.isfile(Path(config.tokenizer_path)):
         # These are for the .json files
-        tokenizer = PreTrainedTokenizerFast.from_pretrained(pretrained_model_name_or_path=config.tokenizer_path)
-        
+        tokenizer = PreTrainedTokenizerFast.from_pretrained(
+            pretrained_model_name_or_path=config.tokenizer_path
+        )
+
     else:
         # These are for the bpe tokenizers
         tokenizer = PreTrainedTokenizerFast.from_pretrained(config.tokenizer_path)
 
     # Build model
-    
+
     # if we are instantiating a new model, we need to instantiate a new model from a json file (if statement)
-    if Path(config.model_path).suffix == '.json':
+    if Path(config.model_path).suffix == ".json":
         model_config = PretrainedConfig.from_json_file(config.model_path)
         model_config.vocab_size = tokenizer.vocab_size
         print(tokenizer.vocab)
-        model_config.pad_token_id = int(tokenizer.vocab['[PAD]'])
-        
+        model_config.pad_token_id = int(tokenizer.vocab["[PAD]"])
+
         special_tokens = {
-        "unk_token": "[UNK]",
-        "cls_token": "[CLS]",
-        "sep_token": "[SEP]",
-        "pad_token": "[PAD]",
-        "mask_token": "[MASK]",
-        "bos_token": "[BOS]",
-        "eos_token": "[EOS]",
-    }
+            "unk_token": "[UNK]",
+            "cls_token": "[CLS]",
+            "sep_token": "[SEP]",
+            "pad_token": "[PAD]",
+            "mask_token": "[MASK]",
+            "bos_token": "[BOS]",
+            "eos_token": "[EOS]",
+        }
 
         # for some reason, we need to add the special tokens even though they are in the json file
         tokenizer.add_special_tokens(special_tokens)
         model = MODEL_DISPATCH[config.model_architecture](model_config)
     else:
-
         # There are different if-else cases because:
         # a) if we have an untrained model, we need to instantiate a new model from a json file (if statement)
         # b) if we have a trained model, we need to load it from a checkpoint (else statement)
-        model = MODEL_DISPATCH[config.model_architecture].from_pretrained(config.model_path)
-
+        model = MODEL_DISPATCH[config.model_architecture].from_pretrained(
+            config.model_path
+        )
 
     # get datasets
-    train_dataset = FastaDataset(config.train_path, num_char_per_token=config.num_char_per_token, convert_to_aa=config.convert_to_aa, tokenizer_type=args.tokenizer_type)
-    eval_dataset = FastaDataset(config.validation_path, num_char_per_token=config.num_char_per_token, convert_to_aa=config.convert_to_aa, tokenizer_type=args.tokenizer_type)
+    train_dataset = FastaDataset(
+        config.train_path,
+        num_char_per_token=config.num_char_per_token,
+        convert_to_aa=config.convert_to_aa,
+        tokenizer_type=args.tokenizer_type,
+    )
+    eval_dataset = FastaDataset(
+        config.validation_path,
+        num_char_per_token=config.num_char_per_token,
+        convert_to_aa=config.convert_to_aa,
+        tokenizer_type=args.tokenizer_type,
+    )
 
     # If the number of tokens in the tokenizer is different from the number of tokens
     # in the model resize the input embedding layer and the MLM prediction head
