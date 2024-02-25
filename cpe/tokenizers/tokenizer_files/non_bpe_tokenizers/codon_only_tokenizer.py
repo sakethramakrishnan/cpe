fd12b0011de81deb9235cad47bdd6013cb5b54ea
sakethramakrishnan <saketh.ramakrishnan@gmail.com>
2024-02-25 07:40:34 -0500

    deleted

--- cpe/tokenizers/tokenizer_files/non_bpe_tokenizers/codon_only_tokenizer.py
+++
@@ -1,6 +0,0 @@
-def codon_only_tokenizer(dna_seq, k=3):
-    tokens = list(dna_seq)
-    grouped_tokens = []
-    for i in range(0, len(tokens), k):
-        grouped_tokens.append("".join(tokens[i : i + k]))
-    return grouped_tokens
